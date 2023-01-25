513d35f10f7e0b121bd1bb6deebc796265149733
Amit Ashok Ohal <amit@Amits-MacBook-Pro.local>
2023-01-25 22:43:43 +0530

    Adding lable markup style

--- CH01-01-hello/hello/main.cpp
+++ CH01-01-hello/hello/main.cpp
@@ -5,7 +5,8 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     QLabel *label=new QLabel("Hello Qt!");
+    QLabel *label1=new QLabel("<h2><i>Hello</i></h2><font color=red> Qt!</font>");
     label->show();
-
+    label1->show();
     return app.exec();
 }
